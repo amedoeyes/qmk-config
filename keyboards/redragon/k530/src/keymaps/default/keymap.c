af1655499c9b74220ab2fdd3c4a310b957efca21
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2026-04-15 01:11:08 +0200

    feat: organize keyboard.json and move custom keycodes to it

--- keyboards/redragon/k530/src/keymaps/default/keymap.c
+++ keyboards/redragon/k530/src/keymaps/default/keymap.c
@@ -8,22 +8,15 @@ typedef enum {
 } layer;
 
 typedef enum {
-    KC_LRESET = SAFE_RANGE,
-    KC_LGUI_OR_F13,
-    KC_RGUI_OR_F13,
-    RM_RST,
-} keycode;
-
-typedef enum {
     TD_LAYERS,
 } tap_dance;
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case KC_LRESET:
+        case KC_RSTL:
             layer_move(L_BASE);
             return false;
-        case KC_LGUI_OR_F13:
+        case KC_LOSS:
             if (record->event.pressed) {
                 if (detected_host_os() == OS_WINDOWS) {
                     register_code(KC_F13);
@@ -38,7 +31,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 }
             }
             return false;
-        case KC_RGUI_OR_F13:
+        case KC_ROSS:
             if (record->event.pressed) {
                 if (detected_host_os() == OS_WINDOWS) {
                     register_code(KC_F13);
@@ -95,23 +88,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                               KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS, //
                               KC_ESC, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,        //
                               KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,          //
-                              KC_LCTL, KC_LGUI_OR_F13, KC_LALT, KC_SPC, KC_RALT, KC_RGUI_OR_F13, KC_RCTL, TD(TD_LAYERS)),
+                              KC_LCTL, KC_LOSS, KC_LALT, KC_SPC, KC_RALT, KC_ROSS, KC_RCTL, TD(TD_LAYERS)),
 
     [L_FN] = LAYOUT_60_ansi(_______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, _______,                     //
                             _______, _______, _______, _______, _______, _______, _______, KC_PGUP, KC_INS, _______, KC_PSCR, _______, _______, _______, //
-                            KC_LRESET, _______, _______, KC_PGDN, _______, _______, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, _______, _______, _______,         //
+                            KC_RSTL, _______, _______, KC_PGDN, _______, _______, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, _______, _______, _______,           //
                             _______, _______, KC_DEL, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   //
                             _______, _______, _______, _______, _______, _______, MO(2), _______),
 
     [L_MOUSE] = LAYOUT_60_ansi(_______, MS_ACL0, MS_ACL1, MS_ACL2, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, //
                                _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, //
-                               KC_LRESET, _______, MS_BTN1, MS_BTN3, MS_BTN2, _______, MS_LEFT, MS_DOWN, MS_UP, MS_RGHT, _______, _______, _______,          //
+                               KC_RSTL, _______, MS_BTN1, MS_BTN3, MS_BTN2, _______, MS_LEFT, MS_DOWN, MS_UP, MS_RGHT, _______, _______, _______,            //
                                _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   //
                                _______, _______, _______, _______, _______, _______, _______, _______),
 
     [L_RGB] = LAYOUT_60_ansi(QK_BOOT, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RM_PREV, RM_NEXT, RM_TOGG, //
                              _______, _______, _______, _______, _______, _______, _______, _______, _______, RM_SPDD, RM_SPDU, RM_HUED, RM_HUEU, RM_RST,  //
-                             KC_LRESET, _______, _______, _______, _______, _______, _______, _______, _______, _______, RM_SATD, RM_SATU, _______,        //
+                             KC_RSTL, _______, _______, _______, _______, _______, _______, _______, _______, _______, RM_SATD, RM_SATU, _______,          //
                              _______, _______, _______, _______, _______, _______, _______, _______, _______, RM_VALD, RM_VALU, _______,                   //
                              _______, _______, _______, _______, _______, _______, _______, _______),
 };
