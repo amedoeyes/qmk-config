bc9666d94537e88c7c3bb22b43cfc3de6be54de6
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2026-04-16 13:15:03 +0200

    feat: replace tap dance layer navigation with a navigation layer

--- keyboards/redragon/k530/src/keymaps/default/keymap.c
+++ keyboards/redragon/k530/src/keymaps/default/keymap.c
@@ -2,14 +2,11 @@
 
 typedef enum {
     L_BASE,
+    L_LNAV,
     L_FN,
     L_MOUSE,
     L_RGB,
 } layer;
-
-typedef enum {
-    TD_LAYERS,
-} tap_dance;
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
@@ -58,20 +55,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     }
 }
 
-void td_layers_finished(tap_dance_state_t *state, void *user_data) {
-    switch (state->count) {
-        case 1:
-            layer_move(L_FN);
-            break;
-        case 2:
-            layer_move(L_MOUSE);
-            break;
-        case 3:
-            layer_move(L_RGB);
-            break;
-    }
-}
-
 const key_override_t *key_overrides[] = {
     &ko_make_basic(MOD_MASK_SHIFT, MS_LEFT, MS_WHLL),
     &ko_make_basic(MOD_MASK_SHIFT, MS_DOWN, MS_WHLD),
@@ -79,32 +62,34 @@ const key_override_t *key_overrides[] = {
     &ko_make_basic(MOD_MASK_SHIFT, MS_RGHT, MS_WHLR),
 };
 
-tap_dance_action_t tap_dance_actions[] = {
-    [TD_LAYERS] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, td_layers_finished, NULL),
-};
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [L_BASE] = LAYOUT_60_ansi(KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC,  //
                               KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS, //
                               KC_ESC, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,        //
                               KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,          //
-                              KC_LCTL, KC_LOSS, KC_LALT, KC_SPC, KC_RALT, KC_ROSS, KC_RCTL, TD(TD_LAYERS)),
+                              KC_LCTL, KC_LOSS, KC_LALT, KC_SPC, KC_RALT, KC_ROSS, KC_RCTL, MO(L_LNAV)),
+
+    [L_LNAV] = LAYOUT_60_ansi(_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,   //
+                              _______, _______, _______, _______, TO(L_RGB), _______, _______, _______, _______, _______, _______, _______, _______, _______, //
+                              TO(L_BASE), _______, _______, _______, TO(L_FN), _______, _______, _______, _______, _______, _______, _______, _______,        //
+                              _______, _______, _______, _______, _______, _______, _______, TO(L_MOUSE), _______, _______, _______, _______,                 //
+                              _______, _______, _______, _______, _______, _______, _______, _______),
 
     [L_FN] = LAYOUT_60_ansi(_______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, _______,                     //
                             _______, _______, _______, _______, _______, _______, _______, KC_PGUP, KC_INS, _______, KC_PSCR, _______, _______, _______, //
-                            KC_RSTL, _______, _______, KC_PGDN, _______, _______, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, _______, _______, _______,           //
+                            _______, _______, _______, KC_PGDN, _______, _______, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, _______, _______, _______,           //
                             _______, _______, KC_DEL, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   //
-                            _______, _______, _______, _______, _______, _______, MO(2), _______),
+                            _______, _______, _______, _______, _______, _______, _______, _______),
 
     [L_MOUSE] = LAYOUT_60_ansi(_______, MS_ACL0, MS_ACL1, MS_ACL2, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, //
                                _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, //
-                               KC_RSTL, _______, MS_BTN1, MS_BTN3, MS_BTN2, _______, MS_LEFT, MS_DOWN, MS_UP, MS_RGHT, _______, _______, _______,            //
+                               _______, _______, MS_BTN1, MS_BTN3, MS_BTN2, _______, MS_LEFT, MS_DOWN, MS_UP, MS_RGHT, _______, _______, _______,            //
                                _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   //
                                _______, _______, _______, _______, _______, _______, _______, _______),
 
     [L_RGB] = LAYOUT_60_ansi(QK_BOOT, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RM_PREV, RM_NEXT, RM_TOGG, //
                              _______, _______, _______, _______, _______, _______, _______, _______, _______, RM_SPDD, RM_SPDU, RM_HUED, RM_HUEU, RM_RST,  //
-                             KC_RSTL, _______, _______, _______, _______, _______, _______, _______, _______, _______, RM_SATD, RM_SATU, _______,          //
+                             _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RM_SATD, RM_SATU, _______,          //
                              _______, _______, _______, _______, _______, _______, _______, _______, _______, RM_VALD, RM_VALU, _______,                   //
                              _______, _______, _______, _______, _______, _______, _______, _______),
 };
